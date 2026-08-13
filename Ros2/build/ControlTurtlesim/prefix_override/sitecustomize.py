a2af59756166f23f51ece4664867ef72b05fe5d9
Mojarras7 <mojarrasalejandro@gmail.com>
2026-08-13 11:33:18 -0600

    chore: add .gitignore and remove build/install/log from repo

--- Ros2/build/ControlTurtlesim/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/media/mojarras/Wiwi/droneops/Learning-Projects/Ros2/install/ControlTurtlesim'
